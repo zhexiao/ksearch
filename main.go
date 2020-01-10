b8b5e3b0f592803aef7e2a21660e844ecf9c6e2a
zhexiao <zhexiao@163.com>
2020-01-10 16:31:39 +0800

    新增测试

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"html/template"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
 	router := gin.Default()
 
-	t, _ := LoadTemplate()
+	t, _ := loadTemplate()
 	router.SetHTMLTemplate(t)
 	router.Static("/static", "./static")
 
@@ -15,4 +18,25 @@ func main() {
 	router.POST("/search", SearchData)
 
 	_ = router.Run(":18888")
+}
+
+//把templates到go文件，参考如下
+//https://github.com/gin-gonic/examples/tree/master/assets-in-binary
+func loadTemplate() (*template.Template, error) {
+	t := template.New("")
+	for name, file := range Assets.Files {
+		if file.IsDir() || !strings.HasSuffix(name, ".html") {
+			continue
+		}
+
+		h, err := ioutil.ReadAll(file)
+		if err != nil {
+			return nil, err
+		}
+		t, err = t.New(name).Parse(string(h))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
 }
